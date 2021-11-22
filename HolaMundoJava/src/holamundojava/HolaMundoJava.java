60cd07808617fc60d55754537c4b1ba377f70440
LuisGuezdez <isguezdez@gmail.com>
2021-11-22 19:29:15 +0100

    Aañadida instrucción que muestre por pantalla Hola Mundo

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,19 @@
+/*
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package holamundojava;
+
+/**
+ *
+ * @author Luis Rodríguez <isguezdez@gmail.com>
+ */
+public class HolaMundoJava 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hola Mundo");
+    }
+    
+}
