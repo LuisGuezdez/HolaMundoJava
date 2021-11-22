cd577963f5d1d23cd8e5a31354c77c82cb060fad
LuisGuezdez <isguezdez@gmail.com>
2021-11-22 19:46:18 +0100

    Añadida instrucción que muestra por pantalla  Adiós Mundo

--- HolaMundoJava/src/holamundojava/HolaMundoJava.java
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -14,6 +14,7 @@ public class HolaMundoJava
     public static void main(String[] args) 
     {
         System.out.println("Hola Mundo");
+        System.out.println("Adiós Mundo");
     }
     
 }
